1f22e737073f05f88df544c28bbdb0623c761f98
Arkiherttua <ainorosah@gmail.com>
2014-03-13 20:53:35 +0200

    voivittu

---
+++ Japanimaatti/src/main/java/arkkis/japanimaatti/App.java
@@ -0,0 +1,13 @@
+package arkkis.japanimaatti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
